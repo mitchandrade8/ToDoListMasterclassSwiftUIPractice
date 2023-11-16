209ee77f3fd8c2f5902af5c6dad947c02deceadc
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-16 10:50:39 -0700

    Initial Commit

---
+++ ToDoListMasterclassSwiftUIPractice/ToDoListMasterclassSwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListMasterclassSwiftUIPracticeApp.swift
+//  ToDoListMasterclassSwiftUIPractice
+//
+//  Created by Mitch Andrade on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListMasterclassSwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
